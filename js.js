a105a9c7fad0f333226fa456d64554effa0bbd43
goooqle <121316502+goooqle@users.noreply.github.com>
2022-12-23 21:04:58 +0100

    init

---
+++ js.js
@@ -0,0 +1,72 @@
+function htmlZad1() {
+    fetch('./tekstovi/zad1html.txt')
+        .then(response => response.text())
+        .then(text => navigator.clipboard.writeText(text));
+} 
+
+function jsZad1() {
+    fetch('./tekstovi/zad1js.txt')
+        .then(response => response.text())
+        .then(text => navigator.clipboard.writeText(text));
+} 
+//
+function htmlZad2() {
+    fetch('./tekstovi/zad2html.txt')
+        .then(response => response.text())
+        .then(text => navigator.clipboard.writeText(text));
+} 
+
+function jsZad2() {
+    fetch('./tekstovi/zad2js.txt')
+        .then(response => response.text())
+        .then(text => navigator.clipboard.writeText(text));
+} 
+//
+function htmlZad3() {
+    fetch('./tekstovi/zad3html.txt')
+        .then(response => response.text())
+        .then(text => navigator.clipboard.writeText(text));
+} 
+
+function jsZad3() {
+    fetch('./tekstovi/zad3js.txt')
+        .then(response => response.text())
+        .then(text => navigator.clipboard.writeText(text));
+} 
+//
+function htmlZad4() {
+    fetch('./tekstovi/zad4html.txt')
+        .then(response => response.text())
+        .then(text => navigator.clipboard.writeText(text));
+} 
+
+function jsZad4() {
+    fetch('./tekstovi/zad4js.txt')
+        .then(response => response.text())
+        .then(text => navigator.clipboard.writeText(text));
+} 
+//
+function htmlZad5() {
+    fetch('./tekstovi/zad5html.txt')
+        .then(response => response.text())
+        .then(text => navigator.clipboard.writeText(text));
+} 
+
+function jsZad5() {
+    fetch('./tekstovi/zad5js.txt')
+        .then(response => response.text())
+        .then(text => navigator.clipboard.writeText(text));
+} 
+//
+function htmlZad6() {
+    fetch('./tekstovi/zad6html.txt')
+        .then(response => response.text())
+        .then(text => navigator.clipboard.writeText(text));
+} 
+
+function jsZad6() {
+    fetch('./tekstovi/zad6js.txt')
+        .then(response => response.text())
+        .then(text => navigator.clipboard.writeText(text));
+} 
+//
